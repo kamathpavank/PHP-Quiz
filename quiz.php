403ea486928f05f0308f49572c01cf8f13b7e5b6
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-10-28 02:44:41 +0530

    Added code for calculating score but giving unexpected if error

--- quiz.php
+++ quiz.php
@@ -8,22 +8,30 @@
         $result = $conn->query($sql);
     ?>
    
-    <form action="action.php" method="post">
+    <form action="quiz.php" method="post">
     <?php if ($result->num_rows > 0) {
+        $count = o;
     // output data of each row
     while($row = $result->fetch_assoc()) { ?>
         <p><?php 
         $question = $row['question'];
         echo "$question" ?></p>
-        <input type="checkbox" value="<?php $row['option1']; ?>" name="option1" id=""><?php $option1= $row['option1']; echo "$option1"?><br>
-        <input type="checkbox" value="<?php $row['option2']; ?>" name="option2" id=""><?php $option2= $row['option2']; echo "$option2"?><br>
-        <input type="checkbox" value="<?php $row['option3']; ?>" name="option3" id=""><?php $option3= $row['option3']; echo "$option3"?><br>
-        <input type="checkbox" value="<?php $row['option4']; ?>" name="option4" id=""><?php $option4= $row['option4']; echo "$option4"?><br>
-    <?php }
+        <input type="checkbox" value="<?php $row['option1']; ?>" name="option" id=""><?php $option1= $row['option1']; echo "$option1"?><br>
+        <input type="checkbox" value="<?php $row['option2']; ?>" name="option" id=""><?php $option2= $row['option2']; echo "$option2"?><br>
+        <input type="checkbox" value="<?php $row['option3']; ?>" name="option" id=""><?php $option3= $row['option3']; echo "$option3"?><br>
+        <input type="checkbox" value="<?php $row['option4']; ?>" name="option" id=""><?php $option4= $row['option4']; echo "$option4"?><br>
+        <input type="submit" value="Submit">
+    <?php 
+    $answer = $_POST['option'];
+    $correctanswer = $row['answer']
+    if($answer == $correctanswer){
+        $count++;
+    }    
     }
-            else{
+     }
+        else{
                 echo "no result";
             }?>
-        <input type="submit" value="Submit">
+        
     </form>
 </div>
