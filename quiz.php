176d625ba52a8ddc1264dbbe82815e021205e791
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-10-28 02:12:55 +0530

    Questions displayed

--- quiz.php
+++ quiz.php
@@ -7,16 +7,18 @@
         $sql = "SELECT * FROM Quiz";
         $result = $conn->query($sql);
     ?>
-    <p><?php $row['question'];?></p>
+   
     <form action="action.php" method="post">
     <?php if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) { ?>
-        <p><?php $row['question']; ?></p>
-        <input type="checkbox" value="<?php $row['option1']; ?>" name="option1" id="">
-        <input type="checkbox" value="<?php $row['option2']; ?>" name="option2" id="">
-        <input type="checkbox" value="<?php $row['option3']; ?>" name="option3" id="">
-        <input type="checkbox" value="<?php $row['option4']; ?>" name="option4" id="">
+        <p><?php 
+        $question = $row['question'];
+        echo "$question" ?></p>
+        <input type="checkbox" value="<?php $row['option1']; ?>" name="option1" id=""><?php $option1= $row['option1']; echo "$option1"?><br>
+        <input type="checkbox" value="<?php $row['option2']; ?>" name="option2" id=""><?php $option2= $row['option2']; echo "$option2"?><br>
+        <input type="checkbox" value="<?php $row['option3']; ?>" name="option3" id=""><?php $option3= $row['option3']; echo "$option3"?><br>
+        <input type="checkbox" value="<?php $row['option4']; ?>" name="option4" id=""><?php $option4= $row['option4']; echo "$option4"?><br>
     <?php }
     }
             else{
