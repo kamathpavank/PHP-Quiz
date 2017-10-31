797c9974926e0dcc3d8f5405779e04f655b58df1
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-10-31 23:34:23 +0530

    added php file for checking answer and made name attribute dynamic

---
+++ check.php
@@ -0,0 +1,10 @@
+<?php
+    require("connect.php");
+?>
+
+<?php
+echo $_POST["1"];
+echo $_POST["2"];
+
+echo $_POST["4"];
+?>
--- quiz.php
+++ quiz.php
@@ -8,30 +8,30 @@
         $result = $conn->query($sql);
     ?>
    
-    <form action="quiz.php" method="post">
+    <form action="check.php" method="post">
     <?php if ($result->num_rows > 0) {
-        $count = o;
+        $count = 0;
     // output data of each row
     while($row = $result->fetch_assoc()) { ?>
         <p><?php 
         $question = $row['question'];
-        echo "$question" ?></p>
-        <input type="checkbox" value="<?php $row['option1']; ?>" name="option" id=""><?php $option1= $row['option1']; echo "$option1"?><br>
-        <input type="checkbox" value="<?php $row['option2']; ?>" name="option" id=""><?php $option2= $row['option2']; echo "$option2"?><br>
-        <input type="checkbox" value="<?php $row['option3']; ?>" name="option" id=""><?php $option3= $row['option3']; echo "$option3"?><br>
-        <input type="checkbox" value="<?php $row['option4']; ?>" name="option" id=""><?php $option4= $row['option4']; echo "$option4"?><br>
-        <input type="submit" value="Submit">
+        echo "$question" ;
+        $quesno = $row['qno'];
+        echo "$quesno";
+       ?></p>
+        <input type="checkbox" value="<?php $row['option1']; ?>" name ="<?php echo "$quesno" ?>" id=""><?php $option1= $row['option1']; echo "$option1"?><br>
+        <input type="checkbox" value="<?php $row['option2']; ?>" name="<?php echo "$quesno" ?>" id=""><?php $option2= $row['option2']; echo "$option2"?><br>
+        <input type="checkbox" value="<?php $row['option3']; ?>" name="<?php echo "$quesno" ?>" id=""><?php $option3= $row['option3']; echo "$option3"?><br>
+        <input type="checkbox" value="<?php $row['option4']; ?>" name="<?php echo "$quesno" ?>"id=""><?php $option4= $row['option4']; echo "$option4"?><br>
+        
     <?php 
-    $answer = $_POST['option'];
-    $correctanswer = $row['answer']
-    if($answer == $correctanswer){
-        $count++;
-    }    
+    
     }
+    
      }
         else{
                 echo "no result";
             }?>
-        
+        <input type="submit" value="Submit">
     </form>
 </div>
