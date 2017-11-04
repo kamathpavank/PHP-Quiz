bf9285c54cda36ca3e7871905e76a4ba4868c4bd
Abhishek Jain <ajj_2396@yahoo.com>
2017-11-04 11:53:27 +0530

    Update quiz.php
    Printing the value to the input tag and Rearranged the code

--- quiz.php
+++ quiz.php
@@ -1,36 +1,48 @@
 <?php
-    require("connect.php");
+require("connect.php");
 ?>
 
 <div>
     <?php
-        $sql = "SELECT * FROM Quiz";
-        $result = $conn->query($sql);
+    $sql = "SELECT * FROM quiz";
+    $result = $conn->query($sql);
     ?>
-   
+
     <form action="check.php" method="post">
-    <?php if ($result->num_rows > 0) {
-        $count = 0;
-    // output data of each row
-    while($row = $result->fetch_assoc()) { ?>
-        <p><?php 
-        $question = $row['question'];
-        echo "$question" ;
-        $quesno = $row['qno'];
-        echo "$quesno";
-       ?></p>
-        
-        <input type="radio" value="<?php $row['option1']; ?>" name ="<?php echo "$quesno" ?>" id=""><?php $option1= $row['option1']; echo "$option1"?><br>
-        <input type="radio" value="<?php $row['option2']; ?>" name="<?php echo "$quesno" ?>" id=""><?php $option2= $row['option2']; echo "$option2"?><br>
-        <input type="radio" value="<?php $row['option3']; ?>" name="<?php echo "$quesno" ?>" id=""><?php $option3= $row['option3']; echo "$option3"?><br>
-        <input type="radio" value="<?php $row['option4']; ?>" name="<?php echo "$quesno" ?>"id=""><?php $option4= $row['option4']; echo "$option4"?><br>
-        
-    <?php 
-    }
-        }
-        else{
-                echo "no result";
-            }?>
+        <?php if ($result->num_rows > 0) {
+            $count = 0;
+            // output data of each row
+            while($row = $result->fetch_assoc()) { 
+                ?>
+                <p>
+                    <?php 
+                    $quesno = $row['qno'];
+                    echo "$quesno]   ";
+                    $question = $row['question'];
+                    echo "$question" ;
+                    ?>
+                </p>
+
+                <input type="radio" value="<?php echo $row['option1']; ?>" name ="<?php echo "$quesno" ?>" id="">
+                <?php $option1= $row['option1']; echo "$option1"?>
+                <br>
+                <input type="radio" value="<?php echo $row['option2']; ?>" name="<?php echo "$quesno" ?>" id="">
+                <?php $option2= $row['option2']; echo "$option2"?>
+                <br>
+                <input type="radio" value="<?php echo $row['option3']; ?>" name="<?php echo "$quesno" ?>" id="">
+                <?php $option3= $row['option3']; echo "$option3"?>
+                <br>
+                <input type="radio" value="<?php echo $row['option4']; ?>" name="<?php echo "$quesno" ?>"id="">
+                <?php $option4= $row['option4']; echo "$option4"?>
+                <br>
+
+                <?php 
+                        }
+                    }
+                    else{
+                        echo "no result";
+                    }
+                ?>
         <input type="submit" value="Submit">
     </form>
 </div>
