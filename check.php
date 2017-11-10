d84db5c4765400934e05760e11cd6952995f7643
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-11-10 09:54:50 +0530

    designed the pages

--- check.php
+++ check.php
@@ -16,25 +16,31 @@ while($row = $result->fetch_assoc()) {
 
 	if(isset($_POST[$i])){
 		$numberofquestons++;
+
+		if($_POST[$i] == $row['answer']){
+			$count+=2;	
+		}
 	}
 	
-	if($_POST[$i] == $row['answer']){
-		$count++;	
-	}
 	$i++;
 }
 
 ?>
 
-<div class="container">
-	<div class="jumbotron" style="text-align: center">
-		<h1 >RESULT</h1>
-		<br>
-		<h2>TOTAL QUESTION ATTEMPTED = <?php echo "$numberofquestons"; ?></h2>
-		<br>
-		<hr>
-		<br>
-		<h2>TOTAL SCORE = <?php echo "$count"; ?></h2>
-	</div>
+<div class="container" style="background-color: #1B4F72;">
+	<div class="jumbotron" style="background-color: #2E86C1;">
+		<div class="container">
+		
+			<div class="jumbotron" style="text-align: center; border: 15px solid  #1B4F72;">
+				<h1 >RESULT</h1>
+				<br>
+				<h2>TOTAL QUESTION ATTEMPTED = <?php echo "$numberofquestons"; ?></h2>
+				<br>
+				<hr style="background-color: #1B4F72; height: 5px;">
+				<br>
+				<h2>TOTAL SCORE = <?php echo "$count"; ?></h2>
+			</div>
 	
+		</div>
+	</div>
 </div>
--- quiz.php
+++ quiz.php
@@ -12,47 +12,52 @@ require("header.php");
     $sql = "SELECT * FROM quiz";
     $result = $conn->query($sql);
     ?>
+    <div class="container" style="background-color: #1B4F72;">
+        <div class="jumbotron" style="background-color: #2E86C1;">
+        <form action="check.php" method="post">
+            <?php if ($result->num_rows > 0) {
+                $count = 0;
+                // output data of each row
+                while($row = $result->fetch_assoc()) { 
+             ?>
+        <!--Questions -->               
+            <div class="container" style="background-color: #1B4F72;">
+                <div class="jumbotron">
+                    <p>
+                        <?php 
+                            $quesno = $row['qno'];
+                            echo "$quesno]   ";
+                            $question = $row['question'];
+                            echo "$question" ;
+                        ?>
+                        <span style="float: right;">2</span>
+                    </p>
 
-    <form action="check.php" method="post">
-        <?php if ($result->num_rows > 0) {
-            $count = 0;
-            // output data of each row
-            while($row = $result->fetch_assoc()) { 
-         ?>
-    <!--Questions -->               
-        <div class="container">
-            <div class="jumbotron">
-                <p>
-                    <?php 
-                        $quesno = $row['qno'];
-                        echo "$quesno]   ";
-                        $question = $row['question'];
-                        echo "$question" ;
-                    ?>
-                </p>
-                <input type="radio" value="<?php echo $row['option1']; ?>" name ="<?php echo "$quesno" ?>" id="">
-                <?php $option1= $row['option1']; echo "$option1"?>
-                <br>
-                <input type="radio" value="<?php echo $row['option2']; ?>" name="<?php echo "$quesno" ?>" id="">
-                <?php $option2= $row['option2']; echo "$option2"?>
-                <br>
-                <input type="radio" value="<?php echo $row['option3']; ?>" name="<?php echo "$quesno" ?>" id="">
-                <?php $option3= $row['option3']; echo "$option3"?>
-                <br>
-                <input type="radio" value="<?php echo $row['option4']; ?>" name="<?php echo "$quesno" ?>"id="">
-                <?php $option4= $row['option4']; echo "$option4"?>
-                <br>
+                    <input type="radio" value="<?php echo $row['option1']; ?>" name ="<?php echo "$quesno" ?>" id="">
+                    <?php $option1= $row['option1']; echo "$option1"?>
+                    <br>
+                    <input type="radio" value="<?php echo $row['option2']; ?>" name="<?php echo "$quesno" ?>" id="">
+                    <?php $option2= $row['option2']; echo "$option2"?>
+                    <br>
+                    <input type="radio" value="<?php echo $row['option3']; ?>" name="<?php echo "$quesno" ?>" id="">
+                    <?php $option3= $row['option3']; echo "$option3"?>
+                    <br>
+                    <input type="radio" value="<?php echo $row['option4']; ?>" name="<?php echo "$quesno" ?>"id="">
+                    <?php $option4= $row['option4']; echo "$option4"?>
+                    <br>
+                </div>
             </div>
-        </div>
-                
-                <?php 
+                    
+                    <?php 
+                            }
                         }
-                    }
-                    else{
-                        echo "no result";
-                    }
-                ?>
+                        else{
+                            echo "no result";
+                        }
+                    ?>
 
-        <input type="submit" value="Submit">
-    </form>
+            <input type="submit" value="Submit" style="background-color: #1B4F72; color: white; ">
+        </form>
+        </div>
+    </div>
 </div>
